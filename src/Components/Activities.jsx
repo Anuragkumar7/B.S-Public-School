4573bdcdaa2cb5d8f33a7032946904414c345cab
Anuragkumar7 <104624067+Anuragkumar7@users.noreply.github.com>
2024-11-08 15:51:44 +0530

    ..

--- src/Components/Activities.jsx
+++ src/Components/Activities.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Activities = () => {
   return (
@@ -140,6 +141,7 @@ const Activities = () => {
           </div>
         </div>
       </section>
+      <Footer/>
     </>
   );
 };
